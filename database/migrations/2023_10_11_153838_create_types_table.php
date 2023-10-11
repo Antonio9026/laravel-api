4583e856d5d9593f7a301d95927afca05868cb7e
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-11 17:40:14 +0200

    corretto nome tab type in types

--- database/migrations/2023_10_11_153838_create_types_table.php
+++ database/migrations/2023_10_11_153838_create_types_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('type', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string("types")->nullable();
+            
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('type');
+        Schema::dropIfExists('types');
     }
 };
