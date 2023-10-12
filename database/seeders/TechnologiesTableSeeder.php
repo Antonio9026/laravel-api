78acbc975f4ca4a27d3d938b6172a9a23274db29
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-12 16:49:21 +0200

    creato migrate model e seeder technologies

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      $technologies = ["JS", "Vue", "PHP", "CSS","HTML", "Bootstrap", "Laravel","NodeJS"];
+    foreach ($technologies as  $technology) {
+        Technology::create(["name"=> $technology]);
+        
+    }
+    }
+}
