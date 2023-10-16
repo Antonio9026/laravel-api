e1ddfb46522024c333390d843e82e99ebf664fc2
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-16 16:37:09 +0200

    cretato rotta e controller api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// rotta api 
+// non diamo un nome alle rotte api perchè non dobbiamo creare nessun link 
+Route::get("projects", [ProjectController::class, "index"]);
